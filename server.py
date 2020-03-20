59a34f5c36985565f2b0d797c43b44a34fe1764e
EC2 Default User <ec2-user@ip-172-31-28-146.ec2.internal>
2020-03-20 02:46:03 +0000

    Add geo APIs

--- server.py
+++ server.py
@@ -1,10 +1,32 @@
 from fastapi import FastAPI
+from arcgis.gis import GIS
+from arcgis.geocoding import geocode, reverse_geocode
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 app = FastAPI()
+gis = GIS()
+            
+@app.get("/place/autocomplete/{search_query}/{current_location}")
+def autocomplete(search_query:str, current_location:str):
+    geocode_result = geocode(address=search_query, as_featureset=True,search_extent=current_location)
+    places = []
+    for place in geocode_result.features:
+        places.append({
+            'name':place.attributes['PlaceName'],
+            'address': place.attributes['Place_addr'],
+            'lat': place.geometry.y,
+            'long': place.geometry.x
+            })
+    return places
 
-
-@app.get("/traces/{timestamp}/{lat}/{long}")
-def get_traces(timestamp: int, lat:float, long:float):
-    return [{'ts':1584066182,'lat':32.769306,'long':32.769306},
-            {'ts':1584066181,'lat':32.769450,'long':-96.779751}
-            ]
+@app.get("/geolocation/{lat}/{long}")
+def geolocation(lat:float, long:float):
+    results = reverse_geocode([long, lat])
+    if results:
+        print(results)
+        return {
+            'name' : results['address']['LongLabel'],
+            'address': results['address']['Address'],
+        }
+    return {}
